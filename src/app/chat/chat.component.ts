3177a51e58d313b243432e17730e7a588369073b
Ene-Daniele <91535355+Ene-Daniele@users.noreply.github.com>
2022-11-27 16:59:33 +0100

    LET MEEE, LET MEEE, LET ME CARVE YOUR WAAAY

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent {
-
+  text = ""
+  ngOnInit(): void {
+    let chatbox = document.getElementById("chatbox") as HTMLInputElement
+    chatbox.addEventListener("keydown", event => {
+      if (event.key == "Enter" && chatbox.value.replaceAll(" ", "") != "") {
+        this.sendMessage(chatbox.value);
+      }
+    })
+  }
+  
+  sendMessage(content: string): void {
+    console.log('Message sent: "' + content + '"')
+    let messages = document.getElementsByClassName("messager");
+    for (let i = 0; i < messages.length; i++) {
+      //move down messages
+    }
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
-
+  @Input() name = "Default User"
+  @Input() pic = "assets/icons/amongus.png"
+  @Input() status = "Empty status"
 }
